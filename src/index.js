9a1f574a65307ea591b83b1f2866c773cd5cfad2
UiVGray <gray.777777@gmail.com>
2018-09-20 17:22:06 +0300

    Task 3:feature

--- src/index.js
+++ src/index.js
@@ -1,30 +1,70 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = new Array();
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    let sortIndices = false;
+    for (let i = 0; i < indices.length-1; i++) {
+      for (let j = i + 1; j < indices.length; j++) {
+        if (indices[i] > indices[j]) {
+          let temp = indices[i];
+          indices[i] = indices[j];
+          indices[j] = temp;
+        }
+      }
+    }
+    if (this.compareFunction !== undefined) { 
+      let testFunction = this.compareFunction(1, 0);
+      let isReverse = false;
+      if (testFunction === -1) {
+        isReverse = true;
+      }
+      for (let i = 0; i < indices.length - 1; i++) {
+        for (let j = i + 1; j < indices.length; j++) {
+          if ((this.compareFunction(this.array[indices[i]], this.array[indices[j]]) > 0 && !isReverse) ||
+              (this.compareFunction(this.array[indices[i]], this.array[indices[j]]) > 0 && isReverse)) {
+            let temp = this.array[indices[j]];
+            this.array[indices[j]] = this.array[indices[i]];
+            this.array[indices[i]] = temp;
+          }else{
+            continue;
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < indices.length-1; i++) {
+        for (let j = i + 1; j < indices.length; j++) {
+          if (this.array[indices[i]] <= this.array[indices[j]]) {
+            continue;
+          }else{
+            let temp = this.array[indices[j]];
+            this.array[indices[j]] = this.array[indices[i]];
+            this.array[indices[i]] = temp;
+          }
+        }
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
